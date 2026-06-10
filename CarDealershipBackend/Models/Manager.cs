e7f9bce047b91deadaeca516ee53ab239fb8b128
winder10fps <samsungplanshetdns19@gmail.com>
2026-06-10 12:43:09 +0300

    Рефакторинг

--- CarDealershipBackend/Models/Manager.cs
+++ CarDealershipBackend/Models/Manager.cs
@@ -3,7 +3,7 @@
     public class Manager
     {
         public int Id { get; set; }
-        public string? Login {  get; set; }
+        public string? Login { get; set; }
         public string? Password { get; set; }
     }
 }
